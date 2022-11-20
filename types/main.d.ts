e30708ec72d099a26460976d9c112666df345852
ehmicky <ehmicky@gmail.com>
2022-11-20 13:25:10 +0100

    Update types documentation

--- types/main.d.ts
+++ types/main.d.ts
@@ -17,14 +17,13 @@ export interface Options {
   readonly stack?: boolean
 
   /**
-   * Log [level](https://github.com/winstonjs/winston#logging-levels).
-   *
-   * @default 'error'
+   * Override the log
+   * [level](https://github.com/winstonjs/winston#logging-levels).
    */
   readonly level?: string
 
   /**
-   *
+   * Maps the `error` before logging it.
    */
   readonly transform?: (error: Error) => Error
 }
@@ -39,27 +38,27 @@ type DynamicOptions = Options | ((error: Error) => Options)
  * [`format`](https://github.com/winstonjs/winston/blob/master/README.md#formats)
  * to [combine](https://github.com/winstonjs/winston#combining-formats) with
  * [`format.simple()`](https://github.com/winstonjs/logform#simple) or
- * [`format.cli()`](https://github.com/winstonjs/logform#cli). This logs
- * only the error name, message and stack, making it useful with
+ * [`format.cli()`](https://github.com/winstonjs/logform#cli). This logs only
+ * the error `name`, `message` and `stack`, making it useful with
  * [transports](https://github.com/winstonjs/winston#transports) like the
  * [console](https://github.com/winstonjs/winston/blob/master/docs/transports.md#console-transport).
  *
  * Errors should be logged using
- * [`logger.error(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
+ * [`logger.*(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
  *
  * @example
  * ```js
  * import { createLogger, transports, format } from 'winston'
  *
  * const logger = createLogger({
- *   format: format.combine(BaseError.shortFormat(), format.cli()),
+ *   format: format.combine(shortFormat(), format.cli()),
  *   transports: [new transports.Console()],
  * })
  *
- * const error = new InputError('Could not read file.', { props: { filePath } })
+ * const error = new ExampleError('Could not read file.')
  * logger.error(error)
  * // Printed on the console:
- * // error: InputError: Could not read file.
+ * // error: ExampleError: Could not read file.
  * //     at ...
  * ```
  */
@@ -76,25 +75,26 @@ export function shortFormat(options?: DynamicOptions): Format
  * [HTTP](https://github.com/winstonjs/winston/blob/master/docs/transports.md#http-transport).
  *
  * Errors should be logged using
- * [`logger.error(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
+ * [`logger.*(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
  *
  * @example
  * ```js
  * import { createLogger, transports, format } from 'winston'
  *
  * const logger = createLogger({
- *   format: format.combine(BaseError.fullFormat(), format.json()),
+ *   format: format.combine(fullFormat(), format.json()),
  *   transports: [new transports.Http(httpOptions)],
  * })
  *
- * const error = new InputError('Could not read file.', { props: { filePath } })
+ * const error = new ExampleError('Could not read file.')
+ * error.filePath = '/...'
  * logger.error(error)
  * // Sent via HTTP:
  * // {
  * //   level: 'error',
- * //   name: 'InputError',
+ * //   name: 'ExampleError',
  * //   message: 'Could not read file.',
- * //   stack: `InputError: Could not read file.
+ * //   stack: `ExampleError: Could not read file.
  * //     at ...`,
  * //   filePath: '/...',
  * // }
