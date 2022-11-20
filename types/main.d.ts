9bbf8d26ded7b65f88d4b1f85bd4996e35afa53b
ehmicky <ehmicky@gmail.com>
2022-11-20 11:48:36 +0100

    Start fixing types

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,12 +1,98 @@
-/**
- * `winston-error-format` options
- */
-export interface Options {}
+import type { Format } from 'logform'
 
 /**
+ * Winston format.
+ */
+export type { Format }
+
+/**
+ * Options of `winston-error-format`
+ */
+export interface Options {
+  /**
+   * Log [level](https://github.com/winstonjs/winston#logging-levels).
+   *
+   * @default 'error'
+   */
+  readonly level?: string
+
+  /**
+   * Whether to log the stack trace.
+   *
+   * @default `true`
+   */
+  readonly stack?: boolean
+
+  /**
+   *
+   */
+  readonly transform?: (error: Error) => Error
+}
+
+/**
+ * Returns a logger
+ * [`format`](https://github.com/winstonjs/winston/blob/master/README.md#formats)
+ * to [combine](https://github.com/winstonjs/winston#combining-formats) with
+ * [`format.simple()`](https://github.com/winstonjs/logform#simple) or
+ * [`format.cli()`](https://github.com/winstonjs/logform#cli). This logs
+ * only the error name, message and stack, making it useful with
+ * [transports](https://github.com/winstonjs/winston#transports) like the
+ * [console](https://github.com/winstonjs/winston/blob/master/docs/transports.md#console-transport).
+ *
+ * Errors should be logged using
+ * [`logger.error(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
  *
  * @example
  * ```js
+ * import { createLogger, transports, format } from 'winston'
+ *
+ * const logger = createLogger({
+ *   format: format.combine(BaseError.shortFormat(), format.cli()),
+ *   transports: [new transports.Console()],
+ * })
+ *
+ * const error = new InputError('Could not read file.', { props: { filePath } })
+ * logger.error(error)
+ * // Printed on the console:
+ * // error: InputError: Could not read file.
+ * //     at ...
  * ```
  */
-export default function winstonErrorFormat(value: any, options?: Options): object
+export function shortFormat(options?: Options): Format
+
+/**
+ * Returns a logger
+ * [`format`](https://github.com/winstonjs/winston/blob/master/README.md#formats)
+ * to [combine](https://github.com/winstonjs/winston#combining-formats) with
+ * [`format.json()`](https://github.com/winstonjs/logform#json) or
+ * [`format.prettyPrint()`](https://github.com/winstonjs/logform#prettyprint).
+ * This logs all error properties, making it useful with
+ * [transports](https://github.com/winstonjs/winston#transports) like
+ * [HTTP](https://github.com/winstonjs/winston/blob/master/docs/transports.md#http-transport).
+ *
+ * Errors should be logged using
+ * [`logger.error(error)`](https://github.com/winstonjs/winston/blob/master/README.md#creating-your-own-logger).
+ *
+ * @example
+ * ```js
+ * import { createLogger, transports, format } from 'winston'
+ *
+ * const logger = createLogger({
+ *   format: format.combine(BaseError.fullFormat(), format.json()),
+ *   transports: [new transports.Http(httpOptions)],
+ * })
+ *
+ * const error = new InputError('Could not read file.', { props: { filePath } })
+ * logger.error(error)
+ * // Sent via HTTP:
+ * // {
+ * //   level: 'error',
+ * //   name: 'InputError',
+ * //   message: 'Could not read file.',
+ * //   stack: `InputError: Could not read file.
+ * //     at ...`,
+ * //   filePath: '/...',
+ * // }
+ * ```
+ */
+export function fullFormat(options?: Options): Format
--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -1,7 +1,43 @@
-import winstonErrorFormat, { Options } from 'winston-error-format'
-import { expectType, expectAssignable } from 'tsd'
+import {
+  expectType,
+  expectAssignable,
+  expectNotAssignable,
+  expectError,
+} from 'tsd'
+import { createLogger } from 'winston'
+import { shortFormat, fullFormat, Format, Options } from 'winston-error-format'
 
-expectType<object>(winstonErrorFormat(true))
+const fFormat = fullFormat()
+const sFormat = shortFormat()
 
-winstonErrorFormat(true, {})
+expectType<Format>(fFormat)
+expectType<Format>(sFormat)
+
+createLogger({ format: fFormat })
+createLogger({ format: sFormat })
+
+fullFormat(undefined)
+shortFormat(undefined)
+fullFormat({})
+shortFormat({})
 expectAssignable<Options>({})
+expectError(fullFormat(true))
+expectError(shortFormat(true))
+expectNotAssignable<Options>(true)
+expectError(fullFormat({ unknown: true }))
+expectError(shortFormat({ unknown: true }))
+expectNotAssignable<Options>({ unknown: true })
+
+fullFormat({ level: 'error' })
+shortFormat({ level: 'error' })
+expectAssignable<Options>({ level: 'error' })
+expectError(fullFormat({ level: true }))
+expectError(shortFormat({ level: true }))
+expectNotAssignable<Options>({ level: true })
+
+fullFormat({ stack: true })
+shortFormat({ stack: true })
+expectAssignable<Options>({ stack: true })
+expectError(fullFormat({ stack: 'true' }))
+expectError(shortFormat({ stack: 'true' }))
+expectNotAssignable<Options>({ stack: 'true' })
